598e192498ed45501de2954731439b30c9d8c3a4
Andrey Yakovenko <yakovenkoandyua@gmail.com>
2026-05-24 12:10:56 +0300

    fix: remove dist from git, Railway builds from source

--- backend/dist/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, _req, res, _next) => {
-    const status = err.status ?? 500;
-    const message = err.message ?? 'Internal Server Error';
-    res.status(status).json({ error: message });
-};
-exports.errorHandler = errorHandler;
--- backend/dist/models/PushSubscription.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const schema = new mongoose_1.Schema({
-    endpoint: { type: String, required: true, unique: true },
-    keys: { p256dh: String, auth: String },
-    userId: { type: String, required: true, index: true },
-}, { timestamps: true });
-exports.default = (0, mongoose_1.model)('PushSubscription', schema);
